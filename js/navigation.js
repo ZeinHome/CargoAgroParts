f85d61de29e5619d30643584a1d5da845764166a
Ilya Pyatnichuk <zein3288@gmail.com>
2023-07-28 01:00:48 +0300

    navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,54 @@
 const arrIcon = [
-  { img: '', img1: '', img2: '', img3: '', img4: '', img5: '', img6: '' },
+  {
+    icon: '../images/navigation/tractor.svg',
+    title: 'Сільгосптехніка',
+    alt: 'brakeDiscs',
+  },
+  {
+    icon: '../images/navigation/truck.svg',
+    title: 'Автотехніка',
+    alt: 'truck',
+  },
+  {
+    icon: '../images/navigation/trailer.svg',
+    title: 'Спецтехніка',
+    alt: 'trailer',
+  },
+  {
+    icon: '../images/navigation/corn.svg',
+    title: 'Посівний матеріал',
+    alt: 'corn',
+  },
+  {
+    icon: '../images/navigation/flour.svg',
+    title: 'Добрива',
+    alt: 'flour',
+  },
+  {
+    icon: '../images/navigation/honey.svg',
+    title: 'Захист рослин',
+    alt: 'honey',
+  },
+  {
+    icon: '../images/navigation/satellite.svg',
+    title: 'Точне землеробство',
+    alt: 'satellite',
+  },
 ];
 
-function name(arrIcon) {}
+const navigation = document.querySelector('.navigation');
+
+function makeNavigation(arrIcon) {
+  return arrIcon
+    .map(obj => {
+      return `<li class="navigation__item">
+    <a href="" class="navigation__item">
+      <img src="${obj.icon}" alt="${obj.alt}" class="navigation__icon" />
+      <h2 class="navigation__title">${obj.title}</h2>
+    </a>
+  </li>`;
+    })
+    .join('');
+}
+
+navigation.insertAdjacentHTML('beforeend', makeNavigation(arrIcon));
