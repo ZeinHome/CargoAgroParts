970263470e6da0012bff83ffdc8894277a638cf8
Ilya Pyatnichuk <zein3288@gmail.com>
2023-08-11 01:34:28 +0300

    contacts

--- js/contacs.js
+++ js/contacs.js
@@ -1,15 +1,12 @@
 (() => {
   const refs = {
     contactBtnRef: document.querySelector('[data-contact-button]'),
-    closeModalBtn: document.querySelector('[data-contact-close]'),
     mobileContactRef: document.querySelector('[data-contact]'),
-    // navigation: document.querySelector('.navigation'),
+    wrapperContact: document.querySelector('.wrapper-contact'),
   };
 
   refs.contactBtnRef.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  //   refs.navigation.addEventListener('click', closeModal);
+  refs.wrapperContact.addEventListener('click', closeModal);
 
   function toggleModal() {
     refs.mobileContactRef.classList.toggle('is-open');
@@ -17,10 +14,11 @@
   }
 
   function closeModal(e) {
-    if (e.target.nodeName !== 'A') {
-      return;
-    }
-    refs.mobileMenuRef.classList.toggle('is-open');
-    document.body.classList.toggle('modal-open');
+    console.log(e.currentTarget);
+    // if (e.target.nodeName !== 'A') {
+    //   return;
+    // }
+    // refs.mobileMenuRef.classList.toggle('is-open');
+    // document.body.classList.toggle('modal-open');
   }
 })();
