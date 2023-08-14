7ec1df44bf4a07c3f39245fa651363d226890620
Ilya Pyatnichuk <zein3288@gmail.com>
2023-08-14 19:57:54 +0300

    close

--- js/contacs.js
+++ js/contacs.js
@@ -2,11 +2,9 @@
   const refs = {
     contactBtnRef: document.querySelector('[data-contact-button]'),
     mobileContactRef: document.querySelector('[data-contact]'),
-    wrapperContact: document.querySelector('.wrapper-contact'),
   };
 
   refs.contactBtnRef.addEventListener('click', toggleModal);
-  refs.wrapperContact.addEventListener('click', closeModal);
 
   function toggleModal() {
     refs.mobileContactRef.classList.toggle('is-open');
