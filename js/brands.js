8cc6d2678f464ef7e5b58baf548468c4e11e8ba1
Ilya Pyatnichuk <zein3288@gmail.com>
2023-08-16 18:36:43 +0300

    brands

---
+++ js/brands.js
@@ -0,0 +1,14 @@
+new Swiper('.brands-swiper', {
+  Autoheight: false,
+  slidesPerView: 1,
+
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 10,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
